b7341ab37fc053b1fa2b1dfe5841451c07194e8a
Nadhif Pratama Subagyo <nadhifps16@gmail.com>
2021-10-01 13:51:36 +0700

    nadhifps16@gmail.com

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Nama: Nadhif Pratama Subagyo");
+		System.out.println("NIM: 6706210072");
+		System.out.println("Kelas: D3RPLA-45-02"); 
+	}
+}	
